09e878191e83f5f5c067159d98bef828f9d845de
zhangli <zhangli_dyx@sina.com>
2019-07-07 17:07:50 +0800

    HelloWorld

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(Strings[] args){
+        System.out.println("Hello World!");
+
+        System.out.println("zhangli   =====>Hello World!");
+
+        System.out.println("3333Hello World!");
+
+    }
+}
